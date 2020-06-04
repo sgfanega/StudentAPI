f56f11ed78269099804918101d14755f3ec1b976
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-04 18:37:42 -0500

    Added GET Methods

--- Data/SqlStudentRepo.cs
+++ Data/SqlStudentRepo.cs
@@ -1,17 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 using StudentAPI.Models;
 
 namespace StudentAPI.Data
 {
     public class SqlStudentRepo : IStudentRepo
     {
+        private readonly StudentContext _studentContext;
+
+        public SqlStudentRepo(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+        
         public IEnumerable<Student> GetAllStudents()
         {
-            throw new System.NotImplementedException();
+            return _studentContext.Students.ToList();
         }
 
         public Student GetStudentById(int studentId)
         {
+            return _studentContext.Students.FirstOrDefault(p => p.StudentId == studentId);
             throw new System.NotImplementedException();
         }
 
