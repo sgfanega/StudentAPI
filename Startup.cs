39d470a9d55bec6930a12ec607fb422a24d5750d
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-04 18:18:23 -0500

    Added AutoMapper

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace StudentAPI
 
             services.AddControllers();
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            
             services.AddScoped<IStudentRepo, SqlStudentRepo>();
         }
 
