87485ca80c6e8ea314e3d87687e0b48bc2a06253
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-04 18:38:10 -0500

    Add Initial StudentReadDto

---
+++ Dtos/StudentReadDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentAPI.Dtos
+{
+    public class StudentReadDto
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int GradeLevel { get; set; }
+    }
+}
