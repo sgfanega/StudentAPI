d79469532d13bf8a93ae00d65bcced41c9b5b52f
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-04 23:12:46 -0500

    Added Initial File

---
+++ Dtos/StudentCreateDto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentAPI.Dtos
+{
+    public class StudentCreateDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        
+        [MaxLength(50)]
+        public string MiddleName { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        
+        [Required]
+        [MaxLength(2)]
+        public int GradeLevel { get; set; }
+        
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
