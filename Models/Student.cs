859c8f80db02bdf8df7692a6ab222acc52454ff6
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-05 13:55:10 -0500

    Updated Student Model

--- Models/Student.cs
+++ Models/Student.cs
@@ -18,11 +18,11 @@ namespace StudentAPI.Models
         [Required]
         [MaxLength(50)]
         public string LastName { get; set; }
-        
+
         [Required]
-        [MaxLength(2)]
-        public int GradeLevel { get; set; }
-        
+        [Range(1, 12)]
+        public int GradeLevel { get; set;}
+
         [Required]
         public DateTime BirthDate { get; set; }
     }
